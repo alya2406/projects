899ef4b2035cddc2afecc4ed34c5637ea1ee3ae9
alya2406 <94674436+alya2406@users.noreply.github.com>
2021-11-19 11:41:20 +0300

    Add files via upload

---
+++ zadacha13 сдано/zadacha13/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadacha13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textSum = textBox1.Text;
+            int p = Convert.ToInt32(textBox1.Text);
+
+            string textSrok = textBox2.Text;
+            int TT = Convert.ToInt32(textBox2.Text);
+            int TTT = TT / 12;
+            int t = TTT + 1;
+
+            int n = Convert.ToInt32(textBox3.Text);
+
+            int sum = p + p * n * t / 100;
+            textBox4.Text = Convert.ToString(sum);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
