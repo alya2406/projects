651936e2028c5ad930e88966aaad455a3b13db26
alya2406 <94674436+alya2406@users.noreply.github.com>
2021-11-27 22:41:56 +0300

    Add files via upload

---
+++ bdkomnatnaya/bdkomnatnaya/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace bdkomnatnaya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFIO = new System.Windows.Forms.TextBox();
+            this.textdolznost = new System.Windows.Forms.TextBox();
+            this.textzp = new System.Windows.Forms.TextBox();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(56, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ФИО сотрудника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(56, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Должность";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(56, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Зарплата";
+            // 
+            // textFIO
+            // 
+            this.textFIO.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textFIO.Location = new System.Drawing.Point(338, 45);
+            this.textFIO.Name = "textFIO";
+            this.textFIO.Size = new System.Drawing.Size(312, 34);
+            this.textFIO.TabIndex = 3;
+            // 
+            // textdolznost
+            // 
+            this.textdolznost.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textdolznost.Location = new System.Drawing.Point(338, 138);
+            this.textdolznost.Name = "textdolznost";
+            this.textdolznost.Size = new System.Drawing.Size(312, 34);
+            this.textdolznost.TabIndex = 4;
+            // 
+            // textzp
+            // 
+            this.textzp.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textzp.Location = new System.Drawing.Point(338, 230);
+            this.textzp.Name = "textzp";
+            this.textzp.Size = new System.Drawing.Size(312, 34);
+            this.textzp.TabIndex = 5;
+            this.textzp.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btninsert
+            // 
+            this.btninsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btninsert.Location = new System.Drawing.Point(61, 336);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(212, 86);
+            this.btninsert.TabIndex = 6;
+            this.btninsert.Text = "Вставить запись";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnexit.Location = new System.Drawing.Point(438, 336);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(212, 86);
+            this.btnexit.TabIndex = 7;
+            this.btnexit.Text = "Закрыть";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.textzp);
+            this.Controls.Add(this.textdolznost);
+            this.Controls.Add(this.textFIO);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFIO;
+        private System.Windows.Forms.TextBox textdolznost;
+        private System.Windows.Forms.TextBox textzp;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnexit;
+    }
+}
+
---
+++ bdkomnatnaya/bdkomnatnaya/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace bdkomnatnaya
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand($"INSERT INTO [Students] (Name, Surname, Birthday) VALUES (@Name, @Surname, @Birthday)", sqlConnection);
+            DateTime birthday = DateTime.Parse(textzp.Text);
+
+
+            command.Parameters.AddWithValue("Name", textFIO.Text);
+            command.Parameters.AddWithValue("Surname", textdolznost.Text);
+            command.Parameters.AddWithValue("Birthday", $"{birthday.Day}/{birthday.Month}/{birthday.Year}");
+
+            MessageBox.Show(command.ExecuteNonQuery().ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\bdkomnatnaya\bdkomnatnaya\Database1.mdf;Integrated Security=True");
+            sqlConnection.Open();
+
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение установлено");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+             
+    }
+    
+
