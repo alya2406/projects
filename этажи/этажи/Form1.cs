eeeffbb7c7abe971e392cbc37f4e9b19f2e083bb
alya2406 <94674436+alya2406@users.noreply.github.com>
2021-12-17 10:48:37 +0300

    Add files via upload

---
+++ этажи/этажи/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace этажи
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                int f = Convert.ToInt32(textBox1.Text);
+                int c = f * 500;
+                textBox2.Text = Convert.ToString(c);
+            }
+            if (radioButton2.Checked)
+            {
+                int f = Convert.ToInt32(textBox1.Text);
+                int c = f * 400;
+                textBox2.Text = Convert.ToString(c);
+            }
+            if (radioButton3.Checked)
+            {
+                int f = Convert.ToInt32(textBox1.Text);
+                int c = f * 350;
+                textBox2.Text = Convert.ToString(c);
+            }
+            
+        }
+    }
+}
