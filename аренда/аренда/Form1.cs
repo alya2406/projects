8265d6e35c15febe9343c6143f93d55f0966d480
alya2406 <94674436+alya2406@users.noreply.github.com>
2021-12-17 11:25:31 +0300

    Add files via upload

---
+++ аренда/аренда/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace аренда
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ch = Convert.ToInt32(textBox2.Text);
+
+            if (radioButton1.Checked) { ch = ch * 500; }
+            if (radioButton2.Checked) { ch = ch * 750; }
+            if (radioButton3.Checked) { ch = ch * 1000;}
+
+            textBox1.Text = Convert.ToString(ch);
+        }
+    }
+}
